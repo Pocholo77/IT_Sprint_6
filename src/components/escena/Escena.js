ef1dd693435758324112b11c34a9cd0b7f571686
Eduardo Wiater <kpooh7766@gmail.com>
2021-07-02 02:05:03 +0200

    Add: Component Escena

---
+++ src/components/escena/Escena.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Escena extends React.Component{
+ 
+    render(){
+        return ( 
+            <div>
+                {<p>El nostre heroi estava surant per lespai sideral quan a la llunyania va albirar una nau espacial</p>}
+            </div>
+        )
+    }
+}
+
+export default Escena
